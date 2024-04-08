8b14315699bcbcdc82bcd071e948a7fb0961b078
Shane Malamut <shanemalamut17@gmail.com>
2024-04-08 15:44:54 -0400

    puzzletilecontent stub

---
+++ puzzlePieces/visual/PuzzleTileContent.java
@@ -0,0 +1,14 @@
+package puzzlePieces.visual;
+
+/**
+ * Presents the information in a PuzzleTile object.
+ * 
+ * @author Shane Malamut, James Madison University
+ * @version 1.0
+ *
+ *          This work complies with the JMU Honor Code.
+ */
+public class PuzzleTileContent
+{
+
+}
