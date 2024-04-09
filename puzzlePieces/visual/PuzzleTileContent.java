b37746c3a2f3debdb6f8c60de6a8995e9401fb21
ShaneMalamut <malamusr@dukes.jmu.edu>
2024-04-08 23:06:24 -0400

    mouselistener thing

--- puzzlePieces/visual/PuzzleTileContent.java
+++ puzzlePieces/visual/PuzzleTileContent.java
@@ -1,5 +1,11 @@
 package puzzlePieces.visual;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import visual.dynamic.described.RuleBasedSprite;
+import visual.statik.TransformableContent;
+
 /**
  * Presents the information in a PuzzleTile object.
  * 
@@ -8,7 +14,35 @@ package puzzlePieces.visual;
  *
  *          This work complies with the JMU Honor Code.
  */
-public class PuzzleTileContent
+public class PuzzleTileContent extends    RuleBasedSprite 
+                               implements MouseMotionListener
 {
+
+  public PuzzleTileContent(TransformableContent content)
+  {
+    super(content);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void mouseDragged(MouseEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void mouseMoved(MouseEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void handleTick(int arg0)
+  {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
